924777cba56a780eaeeae9dcb0f3d9e4e37382f6
Goutham <142166476+Goutham231879@users.noreply.github.com>
2025-01-28 00:02:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+                         #this code creates a window of size 1280 720 until user quit a black window is generated on to the screen in which games are rendered
 pygame.init()
 WINDOW_WIDTH , WINDOW_HEIGHT = 1280 , 720 
 DISPLAY_SURFACE= pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT));
